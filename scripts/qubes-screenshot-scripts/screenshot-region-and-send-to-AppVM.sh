0385573a445af71dd51978aee5c6d36ba41077bb
Aekez <cezgeth@gmail.com>
2018-03-11 22:05:57 +0100

    upload

---
+++ scripts/qubes-screenshot-scripts/screenshot-region-and-send-to-AppVM.sh
@@ -0,0 +1,3 @@
+mv "$(xfce4-screenshooter -ro ls)" ~/Screenshots
+ ( sleep 15 )
+qvm-move-to-vm AppVM ~/Screenshots/Screenshot_*
---
+++ scripts/qubes-screenshot-scripts/screenshot-window-and-send-to-AppVM.sh
@@ -0,0 +1,3 @@
+mv "$(xfce4-screenshooter -wo ls)" ~/Screenshots
+ ( sleep 3 )
+qvm-move-to-vm AppVM ~/Screenshots/screenshot_*
